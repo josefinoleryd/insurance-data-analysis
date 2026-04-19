1ede89f3f2a7f932ae200e32d82ca489c316f764
Josefin <josefin.oleryd@gmail.com>
2026-04-19 19:17:08 +0200

    Skapade nya variabler i 02_prepare_data: bmi_category, age_category, prior_events och risk_score.

--- scripts/02_prepare_data.R
+++ scripts/02_prepare_data.R
@@ -8,41 +8,62 @@ get_mode <- function(x) {
   ux[which.max(tabulate(match(x, ux)))]
 }
 
+# Städa och Feature Engineering i samma pipe
+
 data_clean <- data_raw %>%
   mutate(
     
     # Standardisera text
     across(where(is.character), ~str_to_lower(str_trim(.))),
     
-    # Logiska variabler
-    smoker = recode(smoker, "yes" = TRUE, "no" = FALSE),
-    chronic_condition = recode(chronic_condition, "yes" = TRUE, "no" = FALSE),
-    
     # Hantera saknade värden
     bmi = if_else(
       is.na(bmi),
       median(bmi, na.rm = TRUE),
-      bmi
-    ),
+      bmi),
     annual_checkups = if_else(
       is.na(annual_checkups),
       median(annual_checkups, na.rm = TRUE),
-      annual_checkups
-    ),
+      annual_checkups),
     exercise_level = if_else(
       is.na(exercise_level),
       get_mode(exercise_level),
-      exercise_level
-    ),
+      exercise_level),
+    
+    # Logiska variabler
+    smoker = recode(smoker, "yes" = TRUE, "no" = FALSE),
+    chronic_condition = recode(chronic_condition, "yes" = TRUE, "no" = FALSE),
     
     # Kategoriska variabler till faktorer
     region = as.factor(region),
     exercise_level = as.factor(exercise_level),
     plan_type = as.factor(plan_type),
     sex = as.factor(sex),
-
+    
+    # Nya variabler
+    bmi_category = case_when(
+      bmi < 18.5 ~ "underweight",
+      bmi < 25.0 ~ "normal",
+      bmi < 30.0 ~ "overweight",
+      TRUE ~ "obese"
+    ),
+    age_category = case_when(
+      age < 35 ~ "young",
+      age < 55 ~ "middle-aged",
+      TRUE ~ "senior"
+    ),
+    prior_events = prior_accidents + prior_claims,
+    risk_score = 
+      as.integer(smoker) +
+      as.integer(chronic_condition) +
+      if_else(bmi >= 30, 1, 0),
+    
+    # Göra nya variablerna till faktorer
+    bmi_category = as.factor(bmi_category),
+    age_category = as.factor(age_category)
 )
 
 glimpse(data_clean)
 summary(data_clean)
+str(data_clean)
          
